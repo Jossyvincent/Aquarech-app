5c38edacbfb3427914ac0584d4230c84e092a28e
Vincent Jossy <vincentjossy0@gmail.com>
2025-07-20 14:28:30 +0300

    Redesigned splash screen with ConstraintLayout and added 3s delay

--- app/src/main/java/com/aquarech/farmer/SplashScreenActivity.java
+++ app/src/main/java/com/aquarech/farmer/SplashScreenActivity.java
@@ -3,12 +3,22 @@ package com.aquarech.farmer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.content.Intent;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DELAY = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashScreenActivity.this,OnboardingScreen.class);
+            startActivity(intent);
+            finish();
+        },SPLASH_DELAY);
     }
 }
