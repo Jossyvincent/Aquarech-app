4e7d583eafc0c383fd3046f77169b3e3d25818de
Vincent Jossy <vincentjossy0@gmail.com>
2025-07-25 09:05:53 +0300

    Added the DbHelper Class

--- app/src/main/java/com/aquarech/farmer/db/DbHelper.java
+++ app/src/main/java/com/aquarech/farmer/db/DbHelper.java
@@ -1,4 +1,31 @@
 package com.aquarech.farmer.db;
 
-public class DbHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.aquarech.farmer.utils.Config;
+
+public class DbHelper extends SQLiteOpenHelper {
+    // constructor
+    public DbHelper(Context context) {
+        super(context, Config.DATABASE_NAME, null, Config.DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_USERS_TABLE = "CREATE TABLE " + Config.TABLE_USERS + "("
+                + Config.COLUMN_USER_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + Config.COLUMN_PHONE + " TEXT UNIQUE,"
+                + Config.COLUMN_PASSWORD + " TEXT)";
+        db.execSQL(CREATE_USERS_TABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + Config.TABLE_USERS);
+        onCreate(db);
+    }
+
 }
--- app/src/main/java/com/aquarech/farmer/utils/Config.java
+++ app/src/main/java/com/aquarech/farmer/utils/Config.java
@@ -11,8 +11,17 @@ public class Config {
             "^(?:\\+254|254|0)?(7|1)\\d{8}$"
     );
     // preferenceManager constants
-      public static final String PREF_NAME = "aquarech_prefs";
-      public static final String KEY_HAS_SEEN_INTRO = "has_seen_intro";
+    public static final String PREF_NAME = "aquarech_prefs";
+    public static final String KEY_HAS_SEEN_INTRO = "has_seen_intro";
 
+    // Database constants
+    public static final String DATABASE_NAME= "Aquarech.db";
+    public static final int DATABASE_VERSION = 1;
+
+    // user table constants
+    public static final String TABLE_USERS = "users";
+    public static final String COLUMN_USER_ID = "id";
+    public static final String COLUMN_PHONE = "phone";
+    public static final String COLUMN_PASSWORD = "password";
 
 }
